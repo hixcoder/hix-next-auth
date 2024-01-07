923e384efb1507556d73ed28dfc9b1807db7b681
hixcoder <hixcoder@gmail.com>
2024-01-07 20:36:40 +0100

    finish the credential part

--- actions/register.ts
+++ actions/register.ts
@@ -2,12 +2,29 @@
 
 import { RegisterSchema } from "@/schemas";
 import { z } from "zod";
-
+import bcryptjs from "bcryptjs";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 export async function register(values: z.infer<typeof RegisterSchema>) {
   console.log(values);
   const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcryptjs.hash(password, 10);
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // Todo: send verification token email
+  return { success: "User created!" };
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import LoginButton from "@/components/auth/login-button";
+import { LoginButton } from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
